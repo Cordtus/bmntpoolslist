ea0e56a8b4aa8ef16f521a2e6c8f1bdb87be289a
Cordtus <96965330+Cordtus@users.noreply.github.com>
2024-03-23 15:41:56 -0600

    Update config.js
    add parameter for ibc denom translation anti-ratelimit delay

--- config.js
+++ config.js
@@ -8,8 +8,10 @@ const restAddresses = [
 const config = {
   restAddresses,
   dataPath: './data/pools.json', // Data location
-  initialRetryDelay: 1000, // Initial delay of 1 second in milliseconds
+  initialRetryDelay: 1000, // Initial delay in milliseconds
   maxRetries: 5, // Maximum number of retries
+  includeBaseDenom: true, // Option to include baseDenom field
+  queryDelay: 2000, // Delay between queries in milliseconds
 };
 
 module.exports = config;
